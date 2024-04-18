af1d50dbbc1c700a6a5ce36b0792247e2611f7e6
Morbius00 <rajsaha20026@gmail.com>
2024-04-18 11:53:24 +0530

    feat:A report dilouge box

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+"use client"; // This is a client component 
+import React from 'react'
+import DilougeBox from '../components/dilouge'
+
+export default function page() {
+  return (
+    <div>
+      <DilougeBox />
+    </div>
+  )
+}
---
+++ src/components/dilouge.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function dilouge() {
+  return (
+    <div className='text-xl text-center font-bold'>dilouge</div>
+  )
+}
