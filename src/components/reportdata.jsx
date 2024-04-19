69ad80ee366d492bfbc916c2449083b0ecec48aa
Morbius00 <rajsaha20026@gmail.com>
2024-04-19 19:57:22 +0530

    fix:download button

--- src/components/reportdata.jsx
+++ src/components/reportdata.jsx
@@ -1,33 +1,41 @@
-import React from 'react'
-import {DownloadIcon} from "@/components/assets";
+import React from 'react';
+import { DownloadIcon } from "@/components/assets";
 import Image from 'next/image';
 
 const ReportsData = ({ reports, handleDownload }) => {
-    return (
-        <table className="flex flex-col w-full">
-        <thead className=" bg-slate-200">
-          <tr className="flex lg:space-x-72 md:space-x-64 sm:space-x-40 space-x-24 mx-9">
-            <th>Date</th>
-            <th>Report Name</th>
-            <th>Download</th>
-          </tr>
-        </thead>
-        <tbody>
-          {reports.map((report, index) => (
-            <tr key={index} className="flex flex-row  lg:space-x-[108px] md:space-x-[58px] space-x-2 my-8 mx-5">
-              <td>{report.date}</td>
-              <td>{report.name}</td>
-              <td>
-                  <button onClick={() => handleDownload(report)}>
-                    <Image src={DownloadIcon} alt="Download Icon" width={30} height={30} />
-                  </button>
-                </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    );
+  const handleDownloadAndPrint = (report) => {
+    handleDownload(report); // Initiate file download
+
+    // Wait for a short delay to ensure download completes (adjust delay as needed)
+    setTimeout(() => {
+      window.print(); // Trigger printing
+    }, 1000); // 1000 milliseconds = 1 second
   };
-  
-  export default ReportsData;
-  
+
+  return (
+    <table className="flex flex-col w-full">
+      <thead className="bg-slate-200">
+        <tr className="flex lg:space-x-72 md:space-x-64 sm:space-x-40 space-x-24 mx-9">
+          <th>Date</th>
+          <th>Report Name</th>
+          <th>Download</th>
+        </tr>
+      </thead>
+      <tbody>
+        {reports.map((report, index) => (
+          <tr key={index} className="flex flex-row lg:space-x-[108px] md:space-x-[58px] space-x-2 my-8 mx-5">
+            <td>{report.date}</td>
+            <td>{report.name}</td>
+            <td>
+              <button onClick={() => handleDownloadAndPrint(report)}>
+                <Image src={DownloadIcon} alt="Download Icon" width={30} height={30} />
+              </button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default ReportsData;
